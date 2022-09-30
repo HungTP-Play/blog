e8b2fcb1e34b3d73dd27b165d4ac2a629af4facc
hung.p.tran <hung.p.tran@opswat.com>
2022-09-30 23:16:43 +0700

    Add selection sort

---
+++ src/algorithms/selection_sort.spec.ts
@@ -0,0 +1,38 @@
+import { selectionSort } from './selection_sort'
+
+describe('Test Selection Sort', () => {
+    const cases = [
+        {
+            input: [1, 5, 3, 2, 4],
+            output: [1, 2, 3, 4, 5],
+        },
+        // 20 numbers
+        {
+            input: [
+                1, 5, 3, 2, 4, 6, 12, 78, 34, 23, 45, 67, 89, 90, 56, 43, 21,
+                11, 22, 33,
+            ],
+            output: [
+                1, 2, 3, 4, 5, 6, 11, 12, 21, 22, 23, 33, 34, 43, 45, 56, 67,
+                78, 89, 90,
+            ],
+        },
+        // 30 numbers
+        {
+            input: [
+                1, 5, 3, 2, 4, 6, 12, 78, 34, 23, 45, 67, 89, 90, 56, 43, 21,
+                11, 22, 33, 1, 5, 3, 2, 4, 6, 12, 78, 34, 23,
+            ],
+            output: [
+                1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 11, 12, 12, 21, 22, 23, 23,
+                33, 34, 34, 43, 45, 56, 67, 78, 78, 89, 90,
+            ],
+        },
+    ]
+
+    cases.forEach((c) => {
+        it(`should sort ${c.input} to ${c.output}`, () => {
+            expect(selectionSort(c.input)).toEqual(c.output)
+        })
+    })
+})
---
+++ src/algorithms/selection_sort.ts
@@ -0,0 +1,23 @@
+/**
+ * Sort array using selection sort algorithm (see: https://en.wikipedia.org/wiki/Selection_sort)
+ * @param arr
+ */
+export function selectionSort(arr: number[]): number[] {
+    for (let sorted_index = 0; sorted_index < arr.length - 1; sorted_index++) {
+        let find_index = sorted_index
+        let min_index = sorted_index
+        for (; find_index < arr.length; find_index++) {
+            if (arr[find_index] < arr[min_index]) {
+                min_index = find_index
+            }
+        }
+        swap(sorted_index, min_index, arr)
+    }
+    return arr
+}
+
+export function swap(i: number, j: number, arr: number[]) {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}

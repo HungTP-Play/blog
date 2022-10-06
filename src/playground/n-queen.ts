995132983cb2609b98dd8e0ff70c328e6f4b2572
hung.p.tran <hung.p.tran@opswat.com>
2022-10-06 09:55:39 +0700

    Update N-Queens

--- src/playground/n-queen.ts
+++ src/playground/n-queen.ts
@@ -1,49 +1,74 @@
-/**
- * Solve the N-Queens problem
- * @param n
- */
-export function solveNQueens(n: number): void {
-    const board = new Array(n).fill(0).map(() => new Array(n).fill(0))
-    solveNQueensHelper(board, 0)
+export function buildBoard(n: number): number[][] {
+    return new Array(n).fill(0).map(() => new Array(n).fill(0));
 }
 
-function solveNQueensHelper(board: number[][], row: number): void {
-    if (row === board.length) {
-        console.log(board)
-        return
-    }
-
-    for (let col = 0; col < board.length; col++) {
-        if (isSafe(board, row, col)) {
-            board[row][col] = 1
-            solveNQueensHelper(board, row + 1)
-            board[row][col] = 0
-        }
-    }
-}
-
-// Check if the current position is safe
-function isSafe(board: number[][], row: number, col: number): boolean {
-    // check column
-    for (let i = 0; i < row; i++) {
-        if (board[i][col] === 1) {
-            return false
+export function isSafe(board: number[][], row: number, col: number): boolean {
+    // Check row
+    for (let i = 0; i < col; i++) {
+        if (board[row][i] === 1) {
+            return false;
         }
     }
 
-    // check left diagonal
+    // Check upper diagonal on left side
     for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
         if (board[i][j] === 1) {
-            return false
+            return false;
         }
     }
 
-    // check right diagonal
-    for (let i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+    // Check lower diagonal on left side
+    for (let i = row, j = col; j >= 0 && i < board.length; i++, j--) {
         if (board[i][j] === 1) {
-            return false
+            return false;
         }
     }
 
-    return true
+    return true;
 }
+
+export function placeNQueens(board: number[][], placementCol: number): boolean {
+    if (placementCol === board.length) {
+        return true;
+    }
+
+    for (let row = 0; row < board.length; row++) {
+        if (isSafe(board, row, placementCol)) {
+            board[row][placementCol] = 1;
+            if (placeNQueens(board, placementCol + 1)) {
+                return true;
+            }
+            board[row][placementCol] = 0;
+        }
+    }
+    return false;
+}
+
+/**
+ * Print N-Queen solution and board with '|' and '-' using console.log
+ * @param board
+ */
+export function printNQueens(board: number[][]): void {
+    for (let i = 0; i < board.length; i++) {
+        let row = '';
+        for (let j = 0; j < board.length; j++) {
+            if (board[i][j] === 1) {
+                row += '|Q|';
+            } else {
+                row += '|-|';
+            }
+        }
+        console.log(row);
+    }
+}
+
+export function NQueens(N: number): void {
+    const board = buildBoard(N);
+    if (placeNQueens(board, 0)) {
+        printNQueens(board);
+    } else {
+        console.log('No solution');
+    }
+}
+
+NQueens(12);
